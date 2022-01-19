03dd06d74646b4b1d0b5aa444ef7fc6a7322ce0a
lorent23 <lorenthasanllari23@gmail.com>
2022-01-19 12:07:01 +0100

    4 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -8,5 +8,6 @@ import { Product } from '../products';
 export class ProductAlertsComponent {
   @Input() product: Product | undefined;
   @Output() notify = new EventEmitter();
+  @Output () like = new EventEmitter();
 }
 
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,8 +13,14 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+  
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+  like(){
+    window.alert ('You liked a product');
+  }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
